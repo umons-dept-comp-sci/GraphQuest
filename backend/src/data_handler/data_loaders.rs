3fc532507562cfeb943abbf5e0674d15f2f25456
PizzaManStarted <220584@umons.ac.be>
2024-08-13 11:19:33 +0200

    more work on the importation of datasets

--- backend/src/data_handler/data_loaders.rs
+++ backend/src/data_handler/data_loaders.rs
@@ -9,7 +9,7 @@ use super::super::db_handler::graph_database::*;
 
 /// Creates and stores the content of a `geng` query in the given database, using a set of graph settings.
 /// The table name represents the name of the newly created table.
-pub async fn load_table_with_geng<T: GraphDatabase>(db: &T,nb_of_vertices: u16, graph_settings: &String, edges_born: (Option<u32>, Option<u32>))
+pub async fn load_table_with_geng<T: GraphDatabase>(db: &T,nb_of_vertices: u32, graph_settings: &String, edges_born: (Option<u32>, Option<u32>))
 {
     let mut edges_args = String::new();
     if let (None, Some(nb)) = edges_born 
@@ -29,13 +29,11 @@ pub async fn load_table_with_geng<T: GraphDatabase>(db: &T,nb_of_vertices: u16, 
         }
         edges_args = format!("{min}:{max}");
     }
-    // Call the geng command
+    println!("geng {} {} {} -q", graph_settings, nb_of_vertices, edges_args);
+    // Call the geng com<mand
     let mut call_res = match 
             Command::new("geng")
-                .arg(graph_settings)
-                .arg(nb_of_vertices.to_string())
-                .arg(edges_args)
-                .arg("-q")
+                .arg(format!("{} {} {} -q",  graph_settings, nb_of_vertices, edges_args))
                 .stdout(Stdio::piped()) 
                 .spawn()
 
@@ -43,6 +41,7 @@ pub async fn load_table_with_geng<T: GraphDatabase>(db: &T,nb_of_vertices: u16, 
         Ok(res) => res,
         Err(e) => panic!("There was an error while trying to execute `geng`: {}", e),
     };
+    
     
     {
         let stdout = call_res.stdout.as_mut().unwrap();
@@ -58,6 +57,7 @@ pub async fn load_table_with_geng<T: GraphDatabase>(db: &T,nb_of_vertices: u16, 
 }
 
 
+
 /// Wait for the stdin inputs of the user, reads and stores it in the given database. 
 pub async fn read_pipe_signatures<T: GraphDatabase>(db: &T)
 {
@@ -67,14 +67,12 @@ pub async fn read_pipe_signatures<T: GraphDatabase>(db: &T)
 
 
 
-// TODO add load file method
-#[warn(dead_code)]
-fn read_lines<P>(filename: P) -> Result<Lines<BufReader<File>>>
-where P: AsRef<Path>, {
-    let file = File::open(filename)?;
-    Ok(BufReader::new(file).lines())
+async fn read_file<T:GraphDatabase> (db: &T, path: &String)
+{
+    let f = File::open(path).expect(format!("The given file path \"{path}\" is not valid").as_str());
+    let bufread = BufReader::new(f);
+    db.add_signatures_to_dataset_buffer(bufread).await;
 }
-
 
 
 
@@ -83,7 +81,7 @@ pub enum Method
 {
     /// Read input from GengAPI
     GengAPI{
-        nb_of_vertices: u16,
+        nb_of_vertices: u32,
         graph_settings: String,
         edges_born: (Option<u32>, Option<u32>)
     },
@@ -95,14 +93,14 @@ pub enum Method
 
 impl Method
 {
-    /// Read inpu
+    /// Read and add signatures to the given database using different input methods
     pub async fn read_signatures<T: GraphDatabase>(&self, db:&T)
     {
         match self 
         {
             Method::GengAPI { nb_of_vertices, graph_settings, edges_born } => load_table_with_geng(db, *nb_of_vertices, graph_settings, *edges_born).await,
             Method::Stdin => read_pipe_signatures(db).await,
-            Method::File(_) => todo!(),
+            Method::File(path) => read_file(db, path).await,
         };
     }
 }
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -13,7 +13,7 @@ async fn main() {
     //let wp: Workspace<SqliteGraphDatabase> = Workspace::init_workspace(DB_URL).await;
     let wp : Workspace<SqliteGraphDatabase> = Workspace::connect_workspace(DB_URL).await;
     //wp.add_dataset(GraphQuest::db_handler::data_loaders::Method::Stdin).await;
-    //wp.add_dataset(data_handler::data_loaders::Method::GengAPI { nb_of_vertices: 6, graph_settings: "-c".to_string(), edges_born: (Some(4), None)}).await;
+    wp.add_dataset(data_handler::data_loaders::Method::GengAPI { nb_of_vertices: 6, graph_settings: "-c".to_string(), edges_born: (Some(4), None)}).await;
     wp.close_workspace().await;
 
     
