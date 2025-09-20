346bfb58e95945ff159717b4ac94842885fa97cf
Pizzel <220584@umons.ac.be>
2025-09-20 22:51:56 +0200

    test: added missing topology sort tests

--- gquest_core/tests/invariant_test.rs
+++ gquest_core/tests/invariant_test.rs
@@ -63,6 +63,113 @@ fn new_inv_exec_rely_self_test() {
 
 #[test]
 fn new_correct_inv_order_test() {
+    let (a, b, c) = get_a_b_c_exec();
+
+    let mut order = ExecutableOrderHandler::new();
+    order.add_inv_exec(a.clone()).expect("no issues");
+    order.add_inv_exec(b.clone()).expect("no issues");
+    order.add_inv_exec(c.clone()).expect("no issues");
+
+    let ordered_executables = order.sort().expect("no issues");
+    assert_eq!(c, ordered_executables[0]);
+    assert_eq!(b, ordered_executables[1]);
+    assert_eq!(a, ordered_executables[2]);
+}
+
+#[test]
+fn new_incorrect_inv_test() {
+    let a = InvariantsExecutable::new(
+        VALID_EXEC_A.to_string(),
+        vec!["a".to_string()],
+        vec!["b".to_string(), "c".to_string()],
+    )
+    .expect("Correct inv");
+
+    let b = InvariantsExecutable::new(
+        VALID_EXEC_B.to_string(),
+        vec!["b".to_string(), "a".to_string()],
+        vec!["c".to_string()],
+    )
+    .expect("Correct inv");
+
+    let mut order = ExecutableOrderHandler::new();
+    order.add_inv_exec(a.clone()).expect("no issues");
+    if order.add_inv_exec(a).is_ok() {
+        panic!("should be an error");
+    }
+
+    if order.add_inv_exec(b).is_ok() {
+        panic!("should be an error");
+    }
+}
+
+#[test]
+fn new_cycle_inv_order_test() {
+    let a = InvariantsExecutable::new(
+        VALID_EXEC_A.to_string(),
+        vec!["a".to_string()],
+        vec!["b".to_string(), "c".to_string()],
+    )
+    .expect("Correct inv");
+
+    let b = InvariantsExecutable::new(
+        VALID_EXEC_B.to_string(),
+        vec!["b".to_string()],
+        vec!["c".to_string()],
+    )
+    .expect("Correct inv");
+
+    let c = InvariantsExecutable::new(
+        VALID_EXEC_C.to_string(),
+        vec!["c".to_string()],
+        vec!["a".to_string()],
+    )
+    .expect("Correct inv");
+
+    let mut order = ExecutableOrderHandler::new();
+    order.add_inv_exec(a.clone()).expect("no issues");
+    order.add_inv_exec(b.clone()).expect("no issues");
+    order.add_inv_exec(c.clone()).expect("no issues");
+
+    if order.sort().is_ok() {
+        panic!("Should be cycle error")
+    }
+}
+
+#[test]
+fn new_dep_missing_inv_order_test() {
+    let a = InvariantsExecutable::new(
+        VALID_EXEC_A.to_string(),
+        vec!["a".to_string()],
+        vec!["b".to_string(), "c".to_string()],
+    )
+    .expect("Correct inv");
+
+    let b = InvariantsExecutable::new(
+        VALID_EXEC_B.to_string(),
+        vec!["b".to_string()],
+        vec!["d".to_string()],
+    )
+    .expect("Correct inv");
+
+    let c = InvariantsExecutable::new(VALID_EXEC_C.to_string(), vec!["c".to_string()], vec![])
+        .expect("Correct inv");
+
+    let mut order = ExecutableOrderHandler::new();
+    order.add_inv_exec(a.clone()).expect("no issues");
+    order.add_inv_exec(b.clone()).expect("no issues");
+    order.add_inv_exec(c.clone()).expect("no issues");
+
+    if order.sort().is_ok() {
+        panic!("Should be dep missing error")
+    }
+}
+
+fn get_a_b_c_exec() -> (
+    InvariantsExecutable,
+    InvariantsExecutable,
+    InvariantsExecutable,
+) {
     let a = InvariantsExecutable::new(
         VALID_EXEC_A.to_string(),
         vec!["a".to_string()],
@@ -80,13 +187,5 @@ fn new_correct_inv_order_test() {
     let c = InvariantsExecutable::new(VALID_EXEC_C.to_string(), vec!["c".to_string()], vec![])
         .expect("Correct inv");
 
-    let mut order = ExecutableOrderHandler::new();
-    order.add_inv_exec(a.clone()).expect("no issues");
-    order.add_inv_exec(b.clone()).expect("no issues");
-    order.add_inv_exec(c.clone()).expect("no issues");
-
-    let ordered_executables = order.sort().expect("no issues");
-    assert_eq!(c, ordered_executables[0]);
-    assert_eq!(b, ordered_executables[1]);
-    assert_eq!(a, ordered_executables[2]);
+    (a, b, c)
 }
